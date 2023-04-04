acc677dd2df8aaf46172a4394ba43f3236d5825c
yayinternets <yayinternets+yayinternets@gmail.com>
2023-04-03 23:07:53 -0400

    update update update update update - separated pivottable update - pivottable2 update update update

--- datascripts.js
+++ datascripts.js
@@ -1099,7 +1099,7 @@ pivottable=function(aInputArray, aPivotInstructions, bReplaceColumnNames, bDebug
     return aReturn;
 
 }
-pivottable.sample = function() { return "copy(pivottable.sample())\n\n\n https://pastebin.com/raw/dxkAMSCE \n\n\n https://pastebin.com/raw/8BeaWrTy"; }
+pivottable.sample = function() { return "copy(pivottable.sample()); \n\n\n eval(pivottable.sample()); \n\n\n https://pastebin.com/raw/QMEwQK9W \n\n\n "; }
 
 /* data JMP Tidy.js dplyr and the tidyverse in R */
 
