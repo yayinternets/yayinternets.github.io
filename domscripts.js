3be753e6b7e8a748172dd227d0f18ea8b7ffb34c
yayinternets <yayinternets+yayinternets@gmail.com>
2023-12-07 11:37:47 -0500

    added domscripts_linkify, merged with 2023 unorganized added sTableStyleAssumptions andsTDStyleAssumptions as their own separate variables added additions styles to table and td (assumptions: wanting to make a gs-like grid) added 12/7/2023 - needs refactoring badly added th added 2023 section added "maybe refactor the copyRTFHTML code from https://theoryoftheory.github.io/chunkizeTextarea45000.htm into this page?" to deconstrucDOM() update update update update update update update update wordcloudify w fFunction update update update update update update update update update update update update update update updated toHTMLTable with fourth parameter update

--- domscripts.js
+++ domscripts.js
@@ -1,3 +1,4 @@
+// 2023 unorganized
 function domGenerateTableAndTDWidth(iTableWidth, aColsWidths, sTableIDorClass, iTDHeight) {
     // eg domAutomateTableWidth(1830, [0,0,0,50,0,0,0,0]);
     // this function is necessary because apparently there's no way in css to make a table with a predefined width automatically adjust its tds/columns to be the sum of the table's width without explicitly defining the column widths AND table widths
@@ -36,7 +37,27 @@ function domGenerateTableAndTDWidth(iTableWidth, aColsWidths, sTableIDorClass, i
 
 }
 
-// 2023 unorganized
+function domscripts_linkify(inputText) {
+    
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
+
+
+
 
 // HTMLTABLE TO GOOGLESHEETS DELIMITED COPY-PASTEABLE TABLE: 
 function convertDomTableToGooglesheetsPasteableText(domTable) {
