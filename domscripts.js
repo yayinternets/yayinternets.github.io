bb7e57ad4f1bae3b32fd07eccb218831ee3d548c
yayinternets <yayinternets+yayinternets@gmail.com>
2024-09-14 08:40:36 -0400

    06/18 - made domscript_linkify's regex less strict and matching on any nonspace character (\S) since i need it to match on quotes and foreign letters not just ñ 02/24 - also added dom_displayTDContents from displayTDContents() function 02/24/24 - included domGenerateTableAndTDWidth's sTableIDorClass implememtation finally 12/22 added dom_makeImgAltsHoverableText 12/22 - added function dom_copyHTMLFromClipboardIntoPlaintext() to paste in googlesheets html 12/21 - adding in iPaddingMore to domGenerateTableAndTDWidth() as fifth parameter because on my samsung shitphone the table is too narrow for some reason 12/21/2023 - added dom_copyHTMLToClipboard added .toFixed(2) to parseFloat function added sComment added domscripts_linkify, merged with 2023 unorganized added sTableStyleAssumptions andsTDStyleAssumptions as their own separate variables added additions styles to table and td (assumptions: wanting to make a gs-like grid) added 12/7/2023 - needs refactoring badly added th added 2023 section added "maybe refactor the copyRTFHTML code from https://theoryoftheory.github.io/chunkizeTextarea45000.htm into this page?" to deconstrucDOM() update update update update update update update update wordcloudify w fFunction update update update update update update 06/05/2024 - allowed addEL to have an array of EventListener types (eg ["click", "touchstart"]) and made addELClick that aforementioned array as default update update update update update update update 12/22/23 - renamed HTMLInjecify to HTMLInjectify 12/21/23 - added bAdd_GSDS_Classes parameter to toHTMLTable which lets me choose whether I want to add all the crazy GSDS classes to the table and to each of its td's. NOTE: this would officially break the https://manueldelanda.github.io's tables, so consider going back and linking it all together and changing this parameter to 'true'. Also: this bAdd_GSDS_Classes parameter when false/undefined still results in title='A1' and class='' on each td so consider removing this? idk..lots of work involved in refactoring this in the future to a simpler solution so wait til then? 12/20/23 - removed console.logs from convertValuesOrientedArrayToHTMLTable and convertRecordsOrientedArrayToHTMLTable functions 12/20/23 - added sLineBreak parameter to both convertValuesOrientedArrayToHTMLTable and convertRecordsOrientedArrayToHTMLTable functions, and defaulted it to "<br>". updated toHTMLTable with fourth parameter (iTHRows) update

--- domscripts.js
+++ domscripts.js
@@ -291,6 +291,9 @@ function domscripts_linkify(sInputText, sTitleLogic, bTargetBlank) {
       return `<a href="https://theoryoftheory.github.io/searchtree.htm?search=${sFirstMatch.replaceAll('"', '%22').replaceAll("'", "%27").replaceAll("&", "%26")}">𝓣${sFirstMatch}</a>`;
   });
   
+  // britney?
+  sReturn = sReturn.replaceAll(/y\@(\w+)/g, "<a href='https://www.youtube.com/$1'>@$1</a>");
+  
   // replace the realmarkdown syntax [link name](https://www.link.com) here
   // sReturn = sReturn.replace(replacePattern00, '<img ' + sTargetBlank + 'src="$2">$1</a>');
   replacePattern =  /\[(.*?)\]\((.*?)\)/gim; // via https://stackoverflow.com/questions/37462126/regex-match-markdown-link
@@ -364,8 +367,6 @@ return Array.from(domTable.querySelectorAll("tr")).map(o=>(Array.from(o.querySel
    copy(convertDomTableToGooglesheetsPasteableText(domTable));
    */
 }
-
-
 // dom_2022 "meta-dom" functions - dom_modalize_img_tags,dom_li_paginate, dom_li_paginate_fadeGroup, dom_changeTitleAndFavicon, dom_addImageOverlays, dom_deconstructDOM, dom_load_library
 function dom_modalize_img_tags() {
     // solution from codepen https://codepen.io/RileyB/pen/XQyaXy
@@ -762,8 +763,6 @@ function dom_load_library(sLibrary, bDisplaySample) {
 
 
 
-
-
 // dom_2022 reset2 editors ace vs codeMirror
 
 function editor_ace_ify(e) {
@@ -850,8 +849,6 @@ domLoadScripts_Link(`https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/ad
        
     }, 300)
 }
-
-
 // domBASICscripts => domscripts.serverUNsafe and ES5_UNsafe
 // try { // domscripts.serverUNsafe and ES5_UNsafe
 /* BEGIN - THESE FUNCTIONS SHOULD NEVER BE ADDED TO datascripts.js? */
@@ -1101,9 +1098,7 @@ function dom_BindVariable(sVariable) { // sVariable is the name of the globalvar
 
 
 
-// } catch(e) { console.log("ERROR in domscripts.js " - e) }
-
-// domCOLORS //
+// } catch(e) { console.log("ERROR in domscripts.js " - e) }// domCOLORS //
 
 function getRandomLightColor() {
 // Excellent answer. Worked great for me because I was also wanting to avoid white and grey. – 
@@ -1117,16 +1112,12 @@ function getRandomDarkColor() {
         color += Math.floor(Math.random() * 10);
     }
     return color;
-}
-
-// domDEBUGGINGscripts //
+}// domDEBUGGINGscripts //
 
 var copyS = function(o) { copy(JSON.stringify(o)); }
 var copyF = function(f) { copy( f.toString() ) }
 
-var alertS = function(o) { alert(JSON.stringify(o)); }
-
-// domASCIIscripts (maybe dataASCIIscripts?)
+var alertS = function(o) { alert(JSON.stringify(o)); }// domASCIIscripts (maybe dataASCIIscripts?)
 fStringFromCharCode = function() {
     // js's String.fromCharCode(10) vs gs's CHAR(10)
     // "·".charCodeAt()
@@ -1145,9 +1136,7 @@ fStringFromCharCode = function() {
         "nbsp": `String.fromCharCode("160")`, // CQP.pushToGithub's 
         "bullets": 'String.fromCharCode(8226)+String.fromCharCode(9702)+String.fromCharCode(8226)+String.fromCharCode(8227)+String.fromCharCode(8259)+String.fromCharCode(9675)+String.fromCharCode(9689)+String.fromCharCode(10686)+String.fromCharCode(10687)+String.fromCharCode(164)',
     }
-}
-
-// domJQUERYscripts
+}// domJQUERYscripts
 // beauseph/battk 1 hour ago seems like "define.amd" evaluates to {"JQuery": true}?  I didn't even realize the the Rhino or GraalVM engine is using JQuery? jquery was popular enough when amd was written that it got its own standardized name
 
 // add ajax/xml functionality (or can this be done strictly vanilla?)
@@ -1158,11 +1147,7 @@ fStringFromCharCode = function() {
 
 // add $Hints?
 
-// add $FETCH?  it's just $.ajax(), pretty simple right?
-
-// dom_LZString,Moment,date-fns_scripts?
-
-// dom_WordCloudify
+// add $FETCH?  it's just $.ajax(), pretty simple right?// dom_LZString,Moment,date-fns_scripts?// dom_WordCloudify
 function dom_WordCloudify(aArray, domCanvas, fFunction) {
     if (fFunction) {} else { fFunction = function(a) { alert(a); } }
     // prereqs: <script src="https://cdn.jsdelivr.net/npm/wordcloud@1.2.2/src/wordcloud2.min.js">
@@ -1240,9 +1225,7 @@ function dom_WordCloudify(aArray, domCanvas, fFunction) {
     // redrawButton.addEventListener('click', drawWordCloud);
 
     drawWordCloud();
-}
-
-// dom_D3.js_scripts - where charts, graphs, maps, grams and plots reign supreme
+}// dom_D3.js_scripts - where charts, graphs, maps, grams and plots reign supreme
 // d3_PieChartify, d3_histogramify, d3_barPlotify, d3_StreamGraphify
 
 /* wishlist:
@@ -1715,8 +1698,6 @@ function d3_StreamGraphify(data) {
 
 }
 
-
-
 // dom_highcharts.js_scripts //
 
 function highchartsBarPlotify(data, sTitle, sSubtitle, sYAxis) {
@@ -1777,15 +1758,9 @@ function highchartsBarPlotify(data, sTitle, sSubtitle, sYAxis) {
       }]
     });
 
-}
+}// dom_chart.js_scripts// dom_P5.js_scripts
 
-// dom_chart.js_scripts
-
-// dom_P5.js_scripts
-
-// p5 sound? - https://p5js.org/examples/sound-oscillator-frequency.html?
-
-// domGSDSscripts => NEW googlesheets scripts
+// p5 sound? - https://p5js.org/examples/sound-oscillator-frequency.html?// domGSDSscripts => NEW googlesheets scripts
 // GSDS_CELL, GSDS_RANGE1D, GSDS_RANGE2D, GSDS_CELL_value, GSDS_CELL_valueParseInt, GSDS_RANGE1D_values, GSDS_RANGE2D_values
 // GSDS_getOSR, GSDS_distinguishDomTableAndA1Notation, GSDS_domReplaceAsterisksInA1Notation, GSDS_inputifyTDRANGE, GSDS_eval, GSDS_domTDToA1Notation, GSDS_evalifyTDRANGE
 // domGetTDTextOrValue, domGetTDTextOrValueParseInt, domSetTDTextOrValue
@@ -2177,8 +2152,6 @@ function highchartsBarPlotify(data, sTitle, sSubtitle, sYAxis) {
 
 // } catch(e) { console.log(e); }
 // END NEW googlesheets.scripts.js
-
-
 // dom Spreadsheet functions 
 
 dom_jsSpreadsheetify = function(data, dom) {
@@ -2189,9 +2162,7 @@ dom_jsSpreadsheetify = function(data, dom) {
    jspreadsheet($$$$(dom), {
        data:data,
    });
-}
-
-// domADDELscripts =>
+}// domADDELscripts =>
 
 // DO NOT USE THIS LINE- overrides addEventListener: addEventListener = function(oElements, sType, iIndex, fFunction) { return addEL(oElements, sType, iIndex, fFunction); }
 
@@ -2245,9 +2216,7 @@ function addEL(aElements, aType, eventDotDetail, fFunction) { // vs addEventList
     })
   })
 }
-function addEventListenerClickXYZ(o,i,f) { return addELClick(o,i,f); }
-
-// dom_animate.css_scripts => animate.css
+function addEventListenerClickXYZ(o,i,f) { return addELClick(o,i,f); }// dom_animate.css_scripts => animate.css
     // BEGIN animate.css scripts
     function addAnimateCSSToHover(sSelector, sClass) {  // jQuery-dependent
        sClass = 'animated animate__animated animate__' + sClass; 
@@ -2369,9 +2338,7 @@ function addEventListenerClickXYZ(o,i,f) { return addELClick(o,i,f); }
           });
       }; function $$$a_animate(el,animation,idelay,fFunction1,fFunction2) { return $$$animate(el,animation,idelay,fFunction1,fFunction2); };
       
-    // END animate.css scripts
-
-// domINJECTIFYscripts => domLoadStyles_CSS, domLoadStyles_Link, etc
+    // END animate.css scripts// domINJECTIFYscripts => domLoadStyles_CSS, domLoadStyles_Link, etc
     // 3 SCRIPTS - INJECT STYLES AND SCRIPTS (TO DEPRECATE) 
     domAppendToHead = function(s){ $$$('head')[0].append(s); }
     domAppendStyle = function(e){const t=document.createElement("style");t.textContent=e,document.head.append(t)}; addStyle = domAppendStyle;
@@ -2444,9 +2411,7 @@ function addEventListenerClickXYZ(o,i,f) { return addELClick(o,i,f); }
             }, (document.getElementsByTagName("head")[0] || document.body).appendChild(c)) : n && n()
         }()
     }
-    */
-
-// fauxcopy (aot with copy)
+    */// fauxcopy (aot with copy)
 function fauxcopy(sText){
   var aux = document.createElement("div");
   aux.setAttribute("contentEditable", true);
@@ -2457,9 +2422,7 @@ function fauxcopy(sText){
   aux.focus();
   document.execCommand("copy");
   document.body.removeChild(aux);
-}
-
-// domFETCHscripts => SubmitSuperNinjaForm,fetch_XMLHttpRequest,oGetAllParameters_CLIENT(), oSetAParameter_CLIENT
+}// domFETCHscripts => SubmitSuperNinjaForm,fetch_XMLHttpRequest,oGetAllParameters_CLIENT(), oSetAParameter_CLIENT
 fetch_XMLHttpRequest=function(oHTTPMethodURLPayload) {
     superencode = function (str){  return encodeURIComponent(str).replace(/'/g, "%27"); }
 
@@ -2616,9 +2579,7 @@ function oSetAParameter_CLIENT(oParameters) {
 
     sURL = sURL + "?" + Object.keys(oGetAllParameters_COPY).map(o=>o + "=" + superencode(oGetAllParameters_COPY[o])).join("&");
     return sURL;
-}
-
-// domENCRYPTscripts => superencrypt and decrypt (CryptoJS,LZString)
+}// domENCRYPTscripts => superencrypt and decrypt (CryptoJS,LZString)
 function superencrypt(aVO, sPassword) { // need to fix oo.toString() to JSON.stringify(oo) if I want to convert objects?  or nah?
   // domLoadScripts_Link("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js")
   // copy(superencrypt([["a", "b", "c"],["d - 1", "e - 2", "f - 3"]], "hint"))
@@ -2745,9 +2706,7 @@ function superdecrypt(aVO, sPassword) {
         return sError;
     }
 
-}
-
-// domISLANDSscripts => aGet2DIslands - original es6 version (w/o sample)
+}// domISLANDSscripts => aGet2DIslands - original es6 version (w/o sample)
 // fing charity begins here at StackOverflow - refactor this into datascripts or keep in googlesheets.gs?
 // https://stackoverflow.com/questions/68645601/how-to-extract-a-set-of-isolated-2d-arrays-from-a-larger-2d-array
 function aGet2DIslands (aVO) {
@@ -2844,9 +2803,7 @@ aGet2DIslandsRanges = function(aVO) {
         return sFirstCell + ":" + sLastCell;
     });
     return aVO_A1Notations_Islands_Ranges;
-}
-
-// domDATAHTML.es6.scripts (aka domscripts.2.js)
+}// domDATAHTML.es6.scripts (aka domscripts.2.js)
 /* domDATAHTMLscripts (superset of dataHTMLscripts.js) => datahtmlscripts.js => isomorphic, vanilla, es5-ish datascripts that are related to HTML and datascripts, without needing libraries (the dom, jquery, or lodash */
 // refactor this whole solution into dataDATAHTMLscripts?  or dataHTMLscripts?  why dom?  because es5?
 // note hyperlink() is both html and gs formula related? more functions similar to this concept"?
@@ -3012,9 +2969,7 @@ function HTMLify(aCQPRecordsOriented, bSansHTMLTag) {
 };
 // I removed CQPify polyfill because "CQPify()" is now defined as "HTMLify() with server access"
 // HTMLify alone is itself just a pure vanilla datascript.js function
-// function CQPify(aCQPRecordsOriented) { return HTMLify(aCQPRecordsOriented) };
-
-// domDATAHTML.es5.scripts
+// function CQPify(aCQPRecordsOriented) { return HTMLify(aCQPRecordsOriented) };// domDATAHTML.es5.scripts
 // THE FOLLOWING CODE USED TO BE "domscripts.serversafe", but now its just part of domDATAHTML.es.js scripts
 // pseudocode for new domscript function - refactor convertOSRToHTMLTable, convertRecordsOrientedArrayToHTMLTable, convertValuesOrientedToHTMLTable into one solution? 
 toHTMLSelect=function(aArray, sSelectIDOrClasses, iSelected, bBlank) { // refractor this to accept array of values vs array of objects (select id?)
@@ -3249,9 +3204,7 @@ function hyperlink(sURL, sName, bNoTarget) {
    } else { // googlesheets hyperlink
      return "<a " + ((bNoTarget) ? "": "target='_blank' ") + "href='" + sURL + "'>link</a>";
    }
-}
-
-extractFROMTO = function(sLocations) {
+}extractFROMTO = function(sLocations) {
     sLocations = sLocations.toUpperCase();
     // extractFROMTO("FROM stockland TO dgu");
     // var sLocations = "FROM stockland TO dgu";
